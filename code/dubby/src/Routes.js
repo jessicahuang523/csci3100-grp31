5e31efee47ea1f54d538bdbb7685e17a658a631a
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2020-03-08 12:52:04 +0800

    temporarily redirect route /e to / as event page hasn't been constructed yet

--- code/dubby/src/Routes.js
+++ code/dubby/src/Routes.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RootPage from "./components/RootPage";
 import Signout from "./components/Signout";
@@ -12,6 +17,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" children={<RootPage />} />
         <Route exact path="/g" children={<GymPage />} />
+        <Route exact path="/e" children={<Redirect to="/" />} />
         <Route path="/signout" children={<Signout />} />
       </Switch>
     </Router>
