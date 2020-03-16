cf567baf35bb7f12f4b0b3a8ab0bbfa4872b21d8
Lawrence Tsai <tsailawrence812@gmail.com>
2020-03-16 20:20:12 +0800

    update landing page and package

--- code/dubby/src/components/LandingPage.js
+++ code/dubby/src/components/LandingPage.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "firebase";
+import {Link} from "react-router-dom";
+import {auth} from "firebase";
+import {Button, Form, FormGroup, Label, Input} from "reactstrap";
+import {GoogleLoginButton} from "react-social-login-buttons";
 
 const LandingPage = () => {
-  return (
-    <div className="landing-container">
-      <header>
-        <h1>Dubby</h1>
-        <p>buddy dubby dubby buddy</p>
-      </header>
-      <form onSubmit={e => e.preventDefault()} className="login-form-landing">
-        <input placeholder="username" type="text" />
-        <input placeholder="password" type="password" />
-        <button type="submit">Login</button>
-      </form>
-      <button className="google-login-button">Sign in with Google</button>
+  return (<div className="landing-container">
+    <header>
+      <h1>Dubby</h1>
+      <p>buddy dubby dubby buddy</p>
+    </header>
+    <Form onSubmit={e => e.preventDefault()} className="login-form-landing">
+      <FormGroup>
+        <Label for="username">Username</Label>
+        <Input placeholder="username" type="username"/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Password</Label>
+        <Input placeholder="password" type="password"/>
+      </FormGroup>
+      <Button className="btn-lg btn-dark btn-block" type="submit">Login</Button>
+      <div className="text-center pt-3">
+        Or Sign in with Google
+      </div>
+      <GoogleLoginButton className="mt-3 mb-3"/>
       <Link to="/forgotpassword">Forgot Password?</Link>
       <Link to="/signup">signup</Link>
-      <button onClick={() => auth().signInAnonymously()}>
+      <Button onClick={() => auth().signInAnonymously()}>
         totally for development use anonymous sign in button
-      </button>
-    </div>
-  );
+      </Button>
+    </Form>
+  </div>);
 };
 
 export default LandingPage;
