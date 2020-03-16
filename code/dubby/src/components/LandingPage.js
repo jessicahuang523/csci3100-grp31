c273bfb2dee81c7eae7261472d74f79a8dc56d59
Lawrence Tsai <tsailawrence812@gmail.com>
2020-03-16 20:58:34 +0800

    landing page

--- code/dubby/src/components/LandingPage.js
+++ code/dubby/src/components/LandingPage.js
@@ -5,31 +5,35 @@ import {Button, Form, FormGroup, Label, Input} from "reactstrap";
 import {GoogleLoginButton} from "react-social-login-buttons";
 
 const LandingPage = () => {
-  return (<div className="landing-container">
-    <header>
-      <h1>Dubby</h1>
-      <p>buddy dubby dubby buddy</p>
-    </header>
-    <Form onSubmit={e => e.preventDefault()} className="login-form-landing">
-      <FormGroup>
-        <Label for="username">Username</Label>
-        <Input placeholder="username" type="username"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <Input placeholder="password" type="password"/>
-      </FormGroup>
-      <Button className="btn-lg btn-dark btn-block" type="submit">Login</Button>
-      <div className="text-center pt-3">
-        Or Sign in with Google
-      </div>
-      <GoogleLoginButton className="mt-3 mb-3"/>
-      <Link to="/forgotpassword">Forgot Password?</Link>
-      <Link to="/signup">signup</Link>
-      <Button onClick={() => auth().signInAnonymously()}>
-        totally for development use anonymous sign in button
-      </Button>
-    </Form>
+  return (<div className="page">
+    <div className="landing-container">
+      <header>
+        <h1>Dubby</h1>
+        <p>buddy dubby dubby buddy</p>
+      </header>
+      <Form onSubmit={e => e.preventDefault()} className="login-form-landing">
+        <FormGroup>
+          <Label for="username">Username</Label>
+          <Input placeholder="username" type="username"/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input placeholder="password" type="password"/>
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block" type="submit">Login</Button>
+        <Button color="primary" className="btn-lg btn-block" Link="Link" to="/signup">Sign Up</Button>
+        <div className="text-center pt-3">
+          Or Sign in with Google
+        </div>
+        <GoogleLoginButton className="mt-3 mb-3"/>
+        <div className="text-center pt-3">
+          <Link to="/forgotpassword">Forgot Password?</Link>
+        </div>
+        <Button onClick={() => auth().signInAnonymously()}>
+          totally for development use anonymous sign in button
+        </Button>
+      </Form>
+    </div>
   </div>);
 };
 
