17a1033e21ffebd1714c0fea9d6649d11a00bf6d
jucanalex <jucan.alex95@gmail.com>
2020-03-23 15:50:53 +0200

    make profile page editable and also will persist data into firestore

--- code/dubby/src/components/Profile/ProfilePage.js
+++ code/dubby/src/components/Profile/ProfilePage.js
@@ -5,6 +5,13 @@ import { useParams, Redirect } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
 import NavBar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
+import { Input, Select, Tooltip, Button } from "antd";
+import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
+
+import './ProfilePage.css'
+
+const { Option } = Select;
+const { TextArea } = Input;
 
 const ProfilePage = () => {
   const { uid } = useParams();
@@ -12,20 +19,38 @@ const ProfilePage = () => {
   const { userData, userLoading } = useContext(UserContext);
 
   const [profileData, setProfileData] = useState();
+  const [isEditable, setIsEditable] = useState(false);
 
   useEffect(() => {
     if (userData) {
       const profileDataRef = firestore()
-        .collection("user_profile")
-        .doc(uid ? uid : auth().currentUser.uid);
+          .collection("user_profile")
+          .doc(uid ? uid : auth().currentUser.uid);
       const unsubscribeProfileData = profileDataRef.onSnapshot(snap =>
-        setProfileData(snap.data())
+          setProfileData(snap.data())
       );
       return () => {
         unsubscribeProfileData();
       };
     }
   }, [userData, uid]);
+
+  function enableEditProfile() {
+    setIsEditable(!isEditable);
+  }
+
+  async function persistProfileData() {
+    enableEditProfile();
+    await firestore()
+        .collection("user_profile")
+        .doc(uid ? uid : auth().currentUser.uid)
+        .set(profileData);
+  }
+
+  function updateProfileData(key, value) {
+    const newProfileData = { ...profileData, [key]: value };
+    setProfileData(newProfileData);
+  }
 
   if (userLoading) {
     return <Loading />;
@@ -46,29 +71,40 @@ const ProfilePage = () => {
               style={{ width: "300px", borderRadius: "150px" }}
             />
           </div>
-          <h2 style={{ marginTop: "50px" }}>{profileData.username}</h2>
-          <h4>XXX</h4>
-          <hr style={{ borderTop: "3px solid #D9993C", width: "60%" }} />
-          <p>{profileData.description}</p>
-          <h2 style={{ marginTop: "50px" }}>University</h2>
-          <br />
-          <p>{profileData.university}</p>
-          <hr
-            style={{
-              borderTop: "3px solid #D9993C",
-              width: "90%",
-              margin: "auto"
-            }}
+          {isEditable
+              ? <Button type={'primary'} onClick={() => persistProfileData()}>Save</Button>
+              : <Button type={'dashed'} onClick={() => enableEditProfile()}>Edit</Button>
+          }
+          <h4 style={{ marginTop: "50px" }}>Username:</h4>
+          <Input
+              disabled={!isEditable}
+              onChange={(event) => updateProfileData('username', event.target.value)}
+              value={profileData.username}
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              suffix={
+                <Tooltip title="Username Input">
+                  <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                </Tooltip>
+              }
           />
+          <hr />
+          <h4 style={{ marginTop: "50px" }}>Personal Description:</h4>
+          <TextArea disabled={!isEditable} onChange={(event) => updateProfileData('description', event.target.value)} value={profileData.description} />
+          <hr />
+          <h4 style={{ marginTop: "50px" }}>University:</h4>
+          <Input disabled={!isEditable}
+                 onChange={(event) => updateProfileData('university', event.target.value)}
+                 value={profileData.university}
+          />
+          <hr/>
           <h2 style={{ marginTop: "50px" }}>Sports</h2>
-          <p>{profileData.interested_sports}</p>
-          <hr
-            style={{
-              borderTop: "3px solid #D9993C",
-              width: "90%",
-              margin: "auto"
-            }}
-          />
+          <Input.Group compact>
+            <Select disabled={!isEditable} defaultValue={profileData.interested_sports || 'Select your sport...'} onChange={(value) => updateProfileData('interested_sports', value)}>
+              <Option value="Football">Football</Option>
+              <Option value="Waterpolo">Waterpolo</Option>
+            </Select>
+          </Input.Group>
+          <hr/>
         </div>
       </div>
     );
