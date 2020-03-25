396d7b193630cad720f42ed06d93c2288d87bb50
Lawrence Tsai <tsailawrence812@gmail.com>
2020-03-26 00:20:25 +0800

    minor update

--- code/dubby/src/components/Navbar/Navbar.js
+++ code/dubby/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { auth } from "firebase";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {auth} from "firebase";
 import {
   Collapse,
   Navbar,
@@ -16,48 +16,44 @@ import {
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen(!isOpen);
-
   const handleSignOutClick = () => auth().signOut();
 
-  return (
-    <Navbar color="light" light expand="sm">
-      <NavbarBrand tag={Link} to="/">
-        Dubby
-      </NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
-          <NavItem tag={Link} to="/e">
-            Events
-          </NavItem>
-          <NavItem tag={Link} to="/c">
-            Chat
-          </NavItem>
-          <NavItem tag={Link} to="/g">
-            Gym
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Menu
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem tag={Link} to="/u">
-                Profile
-              </DropdownItem>
-              <DropdownItem tag={Link} to="/friends">
-                Friends
-              </DropdownItem>
-              <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
-                Sign out
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </Collapse>
-    </Navbar>
-  );
+  return (<Navbar color="light" light="light" expand="sm">
+    <NavbarBrand tag={Link} to="/">
+      Dubby
+    </NavbarBrand>
+    <NavbarToggler onClick={toggle}/>
+    <Collapse isOpen={isOpen} navbar="navbar">
+      <Nav className="mr-auto" navbar="navbar">
+        <NavItem tag={Link} to="/e">
+          Events
+        </NavItem>
+        <NavItem tag={Link} to="/c">
+          Chat
+        </NavItem>
+        <NavItem tag={Link} to="/g">
+          Gym
+        </NavItem>
+        <UncontrolledDropdown nav="nav" inNavbar="inNavbar">
+          <DropdownToggle nav="nav" caret="caret">
+            Menu
+          </DropdownToggle>
+          <DropdownMenu right="right">
+            <DropdownItem tag={Link} to="/u">
+              Profile
+            </DropdownItem>
+            <DropdownItem tag={Link} to="/friends">
+              Friends
+            </DropdownItem>
+            <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
+              Sign out
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    </Collapse>
+  </Navbar>);
 };
 
 export default NavBar;
--- code/dubby/src/components/Profile/ProfilePage.js
+++ code/dubby/src/components/Profile/ProfilePage.js
@@ -1,36 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, {useState, useEffect, useContext} from "react";
 import self from "../../self.jpg";
-import { firestore, auth } from "firebase";
-import { useParams, Redirect } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext";
+import {firestore, auth} from "firebase";
+import {useParams, Redirect} from "react-router-dom";
+import {UserContext} from "../../contexts/UserContext";
 import NavBar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
-import { Input, Select, Tooltip, Button } from "antd";
-import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
+import {Input, Select, Tooltip, Button} from "antd";
+import {InfoCircleOutlined, UserOutlined} from "@ant-design/icons";
 
 import "./ProfilePage.css";
-import { sendFriendRequest } from "../../utilityfunctions/Utilities";
+import {sendFriendRequest} from "../../utilityfunctions/Utilities";
 
-const { Option } = Select;
-const { TextArea } = Input;
+const {Option} = Select;
+const {TextArea} = Input;
 
 const ProfilePage = () => {
-  const { uid } = useParams();
+  const {uid} = useParams();
 
-  const { userData, userLoading } = useContext(UserContext);
+  const {userData, userLoading} = useContext(UserContext);
 
   const [profileData, setProfileData] = useState();
   const [isEditable, setIsEditable] = useState(false);
 
   useEffect(() => {
     if (userData) {
-      const profileDataRef = firestore()
-        .collection("user_profile")
-        .doc(uid ? uid : auth().currentUser.uid);
-      const unsubscribeProfileData = profileDataRef.onSnapshot(snap =>
-        setProfileData(snap.data())
-      );
-      return () => {
+      const profileDataRef = firestore().collection("user_profile").doc(
+        uid
+        ? uid
+        : auth().currentUser.uid);
+      const unsubscribeProfileData = profileDataRef.onSnapshot(snap => setProfileData(snap.data()));
+      return() => {
         unsubscribeProfileData();
       };
     }
@@ -42,100 +41,87 @@ const ProfilePage = () => {
 
   async function persistProfileData() {
     enableEditProfile();
-    await firestore()
-      .collection("user_profile")
-      .doc(uid ? uid : auth().currentUser.uid)
-      .set(profileData);
+    await firestore().collection("user_profile").doc(
+      uid
+      ? uid
+      : auth().currentUser.uid).set(profileData);
   }
 
   function updateProfileData(key, value) {
-    const newProfileData = { ...profileData, [key]: value };
+    const newProfileData = {
+      ...profileData,
+      [key]: value
+    };
     setProfileData(newProfileData);
   }
 
   if (userLoading) {
-    return <Loading />;
+    return <Loading/>;
   } else if (!userData) {
-    return <Redirect to="/launch" />;
+    return <Redirect to="/launch"/>;
   } else if (!profileData) {
-    return <Loading />;
+    return <Loading/>;
   } else {
-    return (
-      <div>
-        <NavBar />
-        <div className="profile">
-          <div style={{ textAlign: "center" }}>
-            <img
-              src={self}
-              alt="self"
-              className="resume-pic"
-              style={{ width: "300px", borderRadius: "150px" }}
-            />
-          </div>
-          {uid && uid !== auth().currentUser.uid && (
-            <Button onClick={() => sendFriendRequest({ targetUid: uid })}>
-              add friend
-            </Button>
-          )}
-          {isEditable ? (
-            <Button type={"primary"} onClick={() => persistProfileData()}>
-              Save
-            </Button>
-          ) : (
-            <Button type={"dashed"} onClick={() => enableEditProfile()}>
-              Edit
-            </Button>
-          )}
-          <h4 style={{ marginTop: "50px" }}>Username:</h4>
-          <Input
-            disabled={!isEditable}
-            onChange={event =>
-              updateProfileData("username", event.target.value)
-            }
-            value={profileData.username}
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            suffix={
-              <Tooltip title="Username Input">
-                <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
-              </Tooltip>
-            }
-          />
-          <hr />
-          <h4 style={{ marginTop: "50px" }}>Personal Description:</h4>
-          <TextArea
-            disabled={!isEditable}
-            onChange={event =>
-              updateProfileData("description", event.target.value)
-            }
-            value={profileData.description}
-          />
-          <hr />
-          <h4 style={{ marginTop: "50px" }}>University:</h4>
-          <Input
-            disabled={!isEditable}
-            onChange={event =>
-              updateProfileData("university", event.target.value)
-            }
-            value={profileData.university}
-          />
-          <hr />
-          <h2 style={{ marginTop: "50px" }}>Sports</h2>
-          <Input.Group compact>
-            <Select
-              disabled={!isEditable}
-              defaultValue={
-                profileData.interested_sports || "Select your sport..."
-              }
-              onChange={value => updateProfileData("interested_sports", value)}
-            >
-              <Option value="Football">Football</Option>
-              <Option value="Waterpolo">Waterpolo</Option>
-            </Select>
-          </Input.Group>
-          <hr />
+    return (<div>
+      <NavBar/>
+      <div className="profile">
+        <div style={{
+            textAlign: "center"
+          }}>
+          <img src={self} alt="self" className="resume-pic" style={{
+              width: "300px",
+              borderRadius: "150px"
+            }}/>
         </div>
+        {
+          uid && uid !== auth().currentUser.uid && (<Button onClick={() => sendFriendRequest({targetUid: uid})}>
+            add friend
+          </Button>)
+        }
+        {
+          isEditable
+            ? (<Button type={"primary"} onClick={() => persistProfileData()}>
+              Save
+            </Button>)
+            : (<Button type={"dashed"} onClick={() => enableEditProfile()}>
+              Edit
+            </Button>)
+        }
+        <h2 style={{
+            marginTop: "50px"
+          }}>Username</h2>
+        <hr/>
+        <Input disabled={!isEditable} onChange={event => updateProfileData("username", event.target.value)} value={profileData.username} prefix={<UserOutlined className = "site-form-item-icon" />} suffix={<Tooltip title = "Username Input" > <InfoCircleOutlined style={{
+              color: "rgba(0,0,0,.45)"
+            }}/>
+        </Tooltip>}/>
+
+        <h2 style={{
+            marginTop: "50px"
+          }}>Personal Description</h2>
+
+        <hr/>
+        <TextArea disabled={!isEditable} onChange={event => updateProfileData("description", event.target.value)} value={profileData.description}/>
+        <h2 style={{
+            marginTop: "50px"
+          }}>University</h2>
+        <hr/>
+        <Input disabled={!isEditable} onChange={event => updateProfileData("university", event.target.value)} value={profileData.university}/>
+
+        <h2 style={{
+            marginTop: "50px"
+          }}>Sports</h2>
+        <hr/>
+        <Input.Group compact="compact">
+          <Select disabled={!isEditable} defaultValue={profileData.interested_sports || "Select your sport..."
+} onChange={value => updateProfileData("interested_sports", value)}>
+            <Option value="Football">Football</Option>
+            <Option value="Waterpolo">Waterpolo</Option>
+          </Select>
+        </Input.Group>
+        <br/>
       </div>
-    );
+    </div>);
   }
 };
 export default ProfilePage;
