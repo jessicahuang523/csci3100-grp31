d80ffb6270fca4f56de568da4830c849924ae4ec
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2020-03-26 11:27:09 +0800

    navbar styling tweak

--- code/dubby/src/components/Navbar/Navbar.js
+++ code/dubby/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import {auth} from "firebase";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "firebase";
 import {
   Collapse,
   Navbar,
@@ -11,54 +11,54 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarBrand,
-  Media
+  Media,
+  NavbarBrand
 } from "reactstrap";
 import logo from "../../image/Dubby_logo.png";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggle = () => setIsOpen(!isOpen);
   const handleSignOutClick = () => auth().signOut();
 
-  return (<Navbar color="light" light="light" expand="sm">
-    <img src={logo} style={{
-        width: "200px"
-      }} alt="logo" className="logo"/>
-    <NavbarToggler onClick={toggle}/>
-    <Collapse isOpen={isOpen} navbar="navbar">
-      <Nav className="mr-auto" navbar="navbar">
-        <NavItem tag={Link} to="/">
-          Home
-        </NavItem>
-        <NavItem tag={Link} to="/e">
-          Events
-        </NavItem>
-        <NavItem tag={Link} to="/c">
-          Chat
-        </NavItem>
-        <NavItem tag={Link} to="/g">
-          Gym
-        </NavItem>
-        <UncontrolledDropdown nav="nav" inNavbar="inNavbar">
-          <DropdownToggle nav="nav" caret="caret">
-            Menu
-          </DropdownToggle>
-          <DropdownMenu right="right">
-            <DropdownItem tag={Link} to="/u">
-              Profile
-            </DropdownItem>
-            <DropdownItem tag={Link} to="/friends">
-              Friends
-            </DropdownItem>
-            <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
-              Sign out
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-    </Collapse>
-  </Navbar>);
+  return (
+    <Navbar color="light" light expand="sm">
+      <NavbarBrand tag={Link} to="/">
+        <Media src={logo} style={{ height: "1rem" }} alt="logo" />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem tag={Link} to="/e">
+            Events
+          </NavItem>
+          <NavItem tag={Link} to="/c">
+            Chat
+          </NavItem>
+          <NavItem tag={Link} to="/g">
+            Gym
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Menu
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem tag={Link} to="/u">
+                Profile
+              </DropdownItem>
+              <DropdownItem tag={Link} to="/friends">
+                Friends
+              </DropdownItem>
+              <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
+                Sign out
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
 };
 
 export default NavBar;
