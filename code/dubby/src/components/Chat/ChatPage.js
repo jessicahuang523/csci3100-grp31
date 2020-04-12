649d446381e9c94d6044b58f9c64c81782754d09
黃詠潔 <jessicahuang29@gmail.com>
2020-04-12 23:24:46 +0800

    new chat button

--- code/dubby/src/components/Chat/ChatPage.js
+++ code/dubby/src/components/Chat/ChatPage.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { firestore, auth } from "firebase";
 import { UserContext } from "../../contexts/UserContext";
-import { Jumbotron } from "reactstrap";
+import { Jumbotron, ButtonDropdown,  DropdownToggle, DropdownMenu, DropdownItem} from "reactstrap";
 import Navbar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
 import ChatCard from "./ChatCard";
 
 const ChatPage = () => {
   const { userData, userLoading } = useContext(UserContext);
-
+  const [dropdownOpen, setOpen] = useState(false);
   const [chatList, setChatList] = useState();
+  const toggle = () => setOpen(!dropdownOpen);
 
   useEffect(() => {
     if (userData) {
@@ -40,6 +41,15 @@ const ChatPage = () => {
         <Navbar />
         <Jumbotron>
           <h1>Chats</h1>
+          <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>
+              New Chat
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Private chat</DropdownItem>
+              <DropdownItem>Group chat</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
         </Jumbotron>
         {chatList &&
           chatList.length > 0 &&
