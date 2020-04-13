a01da7e583eb891ba2c0284f795226968c8279da
黃詠潔 <jessicahuang29@gmail.com>
2020-04-13 22:26:10 +0800

    add chat modal

--- code/dubby/src/components/Chat/ChatPage.js
+++ code/dubby/src/components/Chat/ChatPage.js
@@ -2,16 +2,31 @@ import React, { useEffect, useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { firestore, auth } from "firebase";
 import { UserContext } from "../../contexts/UserContext";
-import { Jumbotron, ButtonDropdown,  DropdownToggle, DropdownMenu, DropdownItem} from "reactstrap";
+import { 
+  Jumbotron, 
+  ButtonDropdown,  
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
 import Navbar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
 import ChatCard from "./ChatCard";
 
 const ChatPage = () => {
   const { userData, userLoading } = useContext(UserContext);
-  const [dropdownOpen, setOpen] = useState(false);
   const [chatList, setChatList] = useState();
-  const toggle = () => setOpen(!dropdownOpen);
+  const [dropdownOpen, setOpen] = useState(false);
+  const [modal_private, setModalP] = useState(false);
+  const [modal_group, setModalG] = useState(false);
+  const toggle_dropdown = () => setOpen(!dropdownOpen);
+  const toggle_modal_private = () => setModalP(!modal_private);
+  const toggle_modal_group = () => setModalG(!modal_group);
 
   useEffect(() => {
     if (userData) {
@@ -41,14 +56,34 @@ const ChatPage = () => {
         <Navbar />
         <Jumbotron>
           <h1>Chats</h1>
-          <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+          <ButtonDropdown isOpen={dropdownOpen} toggle={toggle_dropdown}>
             <DropdownToggle caret>
               New Chat
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem>Private chat</DropdownItem>
-              <DropdownItem>Group chat</DropdownItem>
+              <DropdownItem onClick={toggle_modal_private}>Private chat</DropdownItem>
+              <DropdownItem onClick={toggle_modal_group}>Group chat</DropdownItem>
             </DropdownMenu>
+            <Modal isOpen={modal_private} toggle={toggle_modal_private}>
+              <ModalHeader toggle={toggle_modal_private}>Private Chat</ModalHeader>
+              <ModalBody>
+                <h4>To:</h4>
+                friends list
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={toggle_modal_private}>Done</Button> {' '}
+                <Button color="secondary" onClick={toggle_modal_private}>Cancel</Button>
+              </ModalFooter>
+            </Modal>
+            <Modal isOpen={modal_group} toggle={toggle_modal_group}>
+              <ModalHeader toggle={toggle_modal_group}>Group Chat</ModalHeader>
+              <ModalBody>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={toggle_modal_group}>Done</Button> {' '}
+                <Button color="secondary" onClick={toggle_modal_group}>Cancel</Button>
+              </ModalFooter>
+            </Modal>
           </ButtonDropdown>
         </Jumbotron>
         {chatList &&
