63673de550cc9db6c9e99fddfe9d8253643680d5
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2020-04-13 14:38:14 +0800

    search by username and interested sports

--- code/dubby/src/components/Friend/Friend.js
+++ code/dubby/src/components/Friend/Friend.js
@@ -1,13 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Redirect, Link } from "react-router-dom";
+import { firestore } from "firebase";
 import { UserContext } from "../../contexts/UserContext";
 import { FriendContext } from "../../contexts/FriendContext";
+import { EventTypeContext } from "../../contexts/EventTypeContext";
 import {
   Jumbotron,
   ListGroup,
   ListGroupItem,
   ListGroupItemHeading,
   Button,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  DropdownToggle,
+  InputGroupButtonDropdown,
+  DropdownMenu,
+  DropdownItem,
 } from "reactstrap";
 import NavBar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
@@ -22,46 +32,125 @@ const Friend = () => {
   const { sentRequests, receivedRequests, friendList } = useContext(
     FriendContext
   );
+  const { eventTypeData } = useContext(EventTypeContext);
+
+  const [searchUserString, setSearchUserString] = useState();
+  const [searchUserInterest, setSearchUserInterest] = useState();
+  const [searchUserResult, setSearchUserResult] = useState();
+  const [eventTypeDropdownOpen, setEventTypeDropdownOpen] = useState(false);
+
+  const handleToggleEventTypeDropdown = () =>
+    setEventTypeDropdownOpen(!eventTypeDropdownOpen);
+
+  const handleSearchUseSubmit = (e) => {
+    e.preventDefault();
+    const fetchResult = async () => {
+      let query = firestore().collection("user_profile");
+      if (searchUserString) {
+        query = query.where("username", "==", searchUserString);
+      }
+      if (searchUserInterest) {
+        query = query.where(
+          "interested_sports",
+          "array-contains",
+          searchUserInterest
+        );
+      }
+      const res = await query.get();
+      let tmp = [];
+      res.forEach((d) => tmp.push(d.data()));
+      setSearchUserResult(tmp);
+    };
+    if (searchUserInterest || searchUserString) {
+      fetchResult();
+    }
+  };
 
   if (userLoading) {
     return <Loading />;
   } else if (!userData) {
     return <Redirect to="/launch" />;
+  } else if (
+    !sentRequests ||
+    !receivedRequests ||
+    !friendList ||
+    !eventTypeData
+  ) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <NavBar />
+        <Jumbotron>
+          <h1>Friends</h1>
+          <Form onSubmit={handleSearchUseSubmit}>
+            <InputGroup>
+              <Input
+                placeholder="search for a user..."
+                onChange={(e) => setSearchUserString(e.target.value)}
+              />
+              <InputGroupButtonDropdown
+                addonType="append"
+                isOpen={eventTypeDropdownOpen}
+                toggle={handleToggleEventTypeDropdown}
+              >
+                <DropdownToggle caret>
+                  <i className="fas fa-search"></i> Search by{" "}
+                  {searchUserInterest || "interested sports"}
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem onClick={() => setSearchUserInterest(null)}>
+                    None
+                  </DropdownItem>
+                  {eventTypeData.map(({ value, display }) => (
+                    <DropdownItem
+                      key={value}
+                      onClick={() => setSearchUserInterest(display)}
+                    >
+                      {display}
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </InputGroupButtonDropdown>
+              <InputGroupAddon addonType="append">
+                <Button color="primary" type="submit">
+                  <i className="fas fa-search"></i>
+                </Button>
+              </InputGroupAddon>
+            </InputGroup>
+          </Form>
+          <hr />
+          {searchUserResult && <UserList users={searchUserResult} />}
+        </Jumbotron>
+        <UserList
+          users={friendList}
+          heading="Friends"
+          action={unfriendFriend}
+          actionText="Unfriend"
+          actionColor="danger"
+        />
+        <UserList
+          users={sentRequests}
+          heading="Sent Requests"
+          action={removeFriendRequest}
+          actionText="Take Back Request"
+          actionColor="warning"
+        />
+        <UserList
+          users={receivedRequests}
+          heading="Received Requests"
+          action={acceptFriendRequest}
+          actionText="Accept"
+          actionColor="primary"
+        />
+      </div>
+    );
   }
-  return (
-    <div>
-      <NavBar />
-      <Jumbotron>
-        <h1>Friends</h1>
-      </Jumbotron>
-      <UserList
-        users={friendList}
-        heading="Friends"
-        action={unfriendFriend}
-        actionText="Unfriend"
-        actionColor="danger"
-      />
-      <UserList
-        users={sentRequests}
-        heading="Sent Requests"
-        action={removeFriendRequest}
-        actionText="Take Back Request"
-        actionColor="warning"
-      />
-      <UserList
-        users={receivedRequests}
-        heading="Received Requests"
-        action={acceptFriendRequest}
-        actionText="Accept"
-        actionColor="primary"
-      />
-    </div>
-  );
 };
 
 const UserList = ({ users, heading, action, actionText, actionColor }) => (
   <ListGroup>
-    <ListGroupItemHeading>{heading}</ListGroupItemHeading>
+    {heading && <ListGroupItemHeading>{heading}</ListGroupItemHeading>}
     {users && users.length > 0 ? (
       users.map((u) => (
         <ListGroupItem key={u.uid}>
