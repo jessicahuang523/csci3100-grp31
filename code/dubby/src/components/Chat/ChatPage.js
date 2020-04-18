2470a9e014ef6b4da8ef08604bee6d41e58087b0
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2020-04-18 14:13:29 +0800

    clear console error and icon styling

--- code/dubby/src/components/Chat/ChatPage.js
+++ code/dubby/src/components/Chat/ChatPage.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState, useContext} from "react";
-import {Redirect} from "react-router-dom";
-import {firestore} from "firebase";
-import {FriendContext} from "../../contexts/FriendContext";
-import {UserContext} from "../../contexts/UserContext";
+import React, { useEffect, useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import { firestore } from "firebase";
+import { FriendContext } from "../../contexts/FriendContext";
+import { UserContext } from "../../contexts/UserContext";
 import {
   Jumbotron,
   Button,
@@ -13,7 +13,7 @@ import {
   Form,
   FormGroup,
   Input,
-  ButtonGroup
+  ButtonGroup,
 } from "reactstrap";
 import Navbar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
@@ -21,8 +21,8 @@ import ChatCard from "./ChatCard";
 import UserList from "../Friend/UserList";
 
 const ChatPage = () => {
-  const {userData, userLoading} = useContext(UserContext);
-  const {friendListData} = useContext(FriendContext);
+  const { userData, userLoading } = useContext(UserContext);
+  const { friendListData } = useContext(FriendContext);
 
   const [chatList, setChatList] = useState();
   const [privateModalOpen, setPrivateModalOpen] = useState(false);
@@ -31,13 +31,17 @@ const ChatPage = () => {
 
   useEffect(() => {
     if (userData) {
-      const {uid} = userData;
-      const unsubscribeChatList = firestore().collection("user_profile").doc(uid).collection("chats").onSnapshot((snap) => {
-        let tmp = [];
-        snap.forEach((doc) => tmp.push(doc.data()));
-        setChatList(tmp);
-      });
-      return() => {
+      const { uid } = userData;
+      const unsubscribeChatList = firestore()
+        .collection("user_profile")
+        .doc(uid)
+        .collection("chats")
+        .onSnapshot((snap) => {
+          let tmp = [];
+          snap.forEach((doc) => tmp.push(doc.data()));
+          setChatList(tmp);
+        });
+      return () => {
         unsubscribeChatList();
       };
     }
@@ -46,15 +50,15 @@ const ChatPage = () => {
   const handlePrivateModalToggle = () => setPrivateModalOpen(!privateModalOpen);
   const handleGroupModalToggle = () => setGroupModalOpen(!groupModalOpen);
 
-  const handlePrivateSelectFriend = ({targetUid}) => {
+  const handlePrivateSelectFriend = ({ targetUid }) => {
     if (targetUid && friendListData) {
-      const friend = friendListData.find(({uid}) => uid === targetUid);
-      console.log({friend});
+      const friend = friendListData.find(({ uid }) => uid === targetUid);
+      console.log({ friend });
       // TODO: create new chat and redirect to chat page
     }
   };
 
-  const handleSelectFriend = ({targetUid}) => {
+  const handleSelectFriend = ({ targetUid }) => {
     if (targetUid && friendListData) {
       let tmp = selectedFriendData;
       tmp.push(targetUid);
@@ -63,7 +67,7 @@ const ChatPage = () => {
     }
   };
 
-  const handleDeselectFriend = ({targetUid}) => {
+  const handleDeselectFriend = ({ targetUid }) => {
     if (targetUid && selectedFriendData) {
       let tmp = selectedFriendData;
       tmp = tmp.filter((uid) => uid !== targetUid);
@@ -72,79 +76,102 @@ const ChatPage = () => {
   };
 
   if (userLoading || !chatList || !friendListData) {
-    return <Loading/>;
+    return <Loading />;
   } else if (!userData) {
-    return <Redirect to="/launch"/>;
+    return <Redirect to="/launch" />;
   } else {
-    return (<div>
-      <Navbar/>
-      <Jumbotron style={{
-          textAlign: "center"
-        }}>
+    return (
+      <div>
+        <Navbar />
+        <Jumbotron style={{ textAlign: "center" }}>
+          <h1>Chats</h1>
+          <hr />
+          <ButtonGroup>
+            <Button color="primary" onClick={handlePrivateModalToggle}>
+              New Private Chat
+            </Button>
+            <Button color="info" onClick={handleGroupModalToggle}>
+              New Group Chat
+            </Button>
+          </ButtonGroup>
 
-        <h1>Chats</h1>
-        <hr/>
-        <ButtonGroup>
-          <Button color="primary" onClick={handlePrivateModalToggle}>
-            New Private Chat
-          </Button>
-          <Button color="info" onClick={handleGroupModalToggle}>
-            New Group Chat
-          </Button>
-        </ButtonGroup>
+          <Modal isOpen={privateModalOpen} toggle={handlePrivateModalToggle}>
+            <Form>
+              <ModalHeader toggle={handlePrivateModalToggle}>
+                Private Chat
+              </ModalHeader>
+              <ModalBody>
+                <UserList
+                  users={friendListData}
+                  heading="To..."
+                  action={handlePrivateSelectFriend}
+                  actionIcon="fas fa-comment-dots"
+                  actionColor="primary"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="secondary" onClick={handlePrivateModalToggle}>
+                  Done
+                </Button>
+              </ModalFooter>
+            </Form>
+          </Modal>
 
-        <Modal isOpen={privateModalOpen} toggle={handlePrivateModalToggle}>
-          <Form>
-            <ModalHeader toggle={handlePrivateModalToggle}>
-              Private Chat
-            </ModalHeader>
-            <ModalBody>
-              <UserList users={friendListData} heading="To..." action={handlePrivateSelectFriend} actionIcon="fas fa-comment-dots" actionColor="primary"/>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="secondary" onClick={handlePrivateModalToggle}>
-                Done
-              </Button>
-            </ModalFooter>
-          </Form>
-        </Modal>
+          <Modal isOpen={groupModalOpen} toggle={handleGroupModalToggle}>
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                // TODO: create group chat and redirect to chat page
+              }}
+            >
+              <ModalHeader toggle={handleGroupModalToggle}>
+                Group Chat
+              </ModalHeader>
+              <ModalBody>
+                <FormGroup>
+                  <h4>Group name:</h4>
+                  <Input required="required" type="text" id="groupChatName" />
+                </FormGroup>
+                <UserList
+                  users={friendListData.filter(
+                    ({ uid }) => selectedFriendData.indexOf(uid) > -1
+                  )}
+                  heading="Participants"
+                  action={handleDeselectFriend}
+                  actionIcon="fas fa-minus"
+                  actionColor="warning"
+                />
+                <hr />
+                <UserList
+                  users={friendListData.filter(
+                    ({ uid }) => selectedFriendData.indexOf(uid) < 0
+                  )}
+                  heading="Add Users"
+                  action={handleSelectFriend}
+                  actionIcon="fas fa-plus"
+                  actionColor="primary"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" color="primary">
+                  <i className="fas fa-comment-dots"></i>
+                  Create Chat!
+                </Button>{" "}
+                <Button color="danger" outline onClick={handleGroupModalToggle}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </Form>
+          </Modal>
+        </Jumbotron>
 
-        <Modal isOpen={groupModalOpen} toggle={handleGroupModalToggle}>
-          <Form onSubmit={(e) => {
-              e.preventDefault();
-              // TODO: create group chat and redirect to chat page
-            }}>
-            <ModalHeader toggle={handleGroupModalToggle}>
-              Group Chat
-            </ModalHeader>
-            <ModalBody>
-              <FormGroup>
-                <h4>Group name:</h4>
-                <Input required="required" type="text" id="groupChatName"/>
-              </FormGroup>
-              <UserList users={friendListData.filter(({uid}) => selectedFriendData.indexOf(uid) > -1)} heading="Participants" action={handleDeselectFriend} actionIcon="fas fa-minus" actionColor="warning"/>
-              <hr/>
-              <UserList users={friendListData.filter(({uid}) => selectedFriendData.indexOf(uid) < 0)} heading="Add Users" action={handleSelectFriend} actionIcon="fas fa-plus" actionColor="primary"/>
-            </ModalBody>
-            <ModalFooter>
-              <Button type="submit" color="primary">
-                <i className="fas fa-comment-dots"></i>
-                Create Chat!
-              </Button>{" "}
-              <Button color="danger" outline="outline" onClick={handleGroupModalToggle}>
-                Cancel
-              </Button>
-            </ModalFooter>
-          </Form>
-        </Modal>
-      </Jumbotron>
-
-      <div style={{
-          padding: "1rem"
-        }}>
-        {chatList && chatList.length > 0 && chatList.map((chat) => <ChatCard key={chat.cid} cid={chat.cid}/>)}
+        <div style={{ padding: "1rem" }}>
+          {chatList &&
+            chatList.length > 0 &&
+            chatList.map((chat) => <ChatCard key={chat.cid} cid={chat.cid} />)}
+        </div>
       </div>
-    </div>);
+    );
   }
 };
 
--- code/dubby/src/components/Navbar/Navbar.js
+++ code/dubby/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import {auth} from "firebase";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "firebase";
 import {
   Collapse,
   Navbar,
@@ -13,7 +13,7 @@ import {
   DropdownItem,
   Media,
   NavbarBrand,
-  NavLink
+  NavLink,
 } from "reactstrap";
 import logo from "../../image/Dubby_logo.png";
 
@@ -23,49 +23,49 @@ const NavBar = () => {
   const toggle = () => setIsOpen(!isOpen);
   const handleSignOutClick = () => auth().signOut();
 
-  return (<Navbar color="light" light="light" expand="sm">
-    <NavbarBrand tag={Link} to="/">
-      <Media src={logo} style={{
-          height: "1rem"
-        }} alt="logo"/>
-    </NavbarBrand>
-    <NavbarToggler onClick={toggle}/>
-    <Collapse isOpen={isOpen} navbar="navbar">
-      <Nav className="mr-auto" navbar="navbar">
-        <NavItem>
-          <NavLink tag={Link} to="/e">
-            Events
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to="/c">
-            Chat
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to="/g">
-            Gym
-          </NavLink>
-        </NavItem>
-        <UncontrolledDropdown nav="nav" inNavbar="inNavbar">
-          <DropdownToggle nav="nav" caret="caret">
-            Menu
-          </DropdownToggle>
-          <DropdownMenu right="right">
-            <DropdownItem tag={Link} to="/u">
-              Profile
-            </DropdownItem>
-            <DropdownItem tag={Link} to="/friends">
-              Friends
-            </DropdownItem>
-            <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
-              Sign out
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-    </Collapse>
-  </Navbar>);
+  return (
+    <Navbar color="light" light expand="sm">
+      <NavbarBrand tag={Link} to="/">
+        <Media src={logo} style={{ height: "1rem" }} alt="logo" />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to="/e">
+              Events
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/c">
+              Chat
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/g">
+              Gym
+            </NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Menu
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem tag={Link} to="/u">
+                Profile
+              </DropdownItem>
+              <DropdownItem tag={Link} to="/friends">
+                Friends
+              </DropdownItem>
+              <DropdownItem tag={Link} to="/" onClick={handleSignOutClick}>
+                Sign out
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
 };
 
 export default NavBar;
