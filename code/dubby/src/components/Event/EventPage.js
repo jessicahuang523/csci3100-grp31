62aed93bb7d92a5cc72a63f9a1b520cd409dceda
Lawrence Tsai <tsailawrence812@gmail.com>
2020-04-18 15:41:50 +0800

    color

--- code/dubby/src/components/Event/EventPage.js
+++ code/dubby/src/components/Event/EventPage.js
@@ -1,63 +1,64 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Redirect, Link } from "react-router-dom";
-import { firestore } from "firebase";
-import { UserContext } from "../../contexts/UserContext";
-import { Jumbotron, Button } from "reactstrap";
+import React, {useState, useEffect, useContext} from "react";
+import {Redirect, Link} from "react-router-dom";
+import {firestore} from "firebase";
+import {UserContext} from "../../contexts/UserContext";
+import {Jumbotron, Button} from "reactstrap";
 import NavBar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
 import EventCard from "./EventCard";
 
 const EventPage = () => {
-  const { userData, userLoading } = useContext(UserContext);
+  const {userData, userLoading} = useContext(UserContext);
 
   const [userEventList, setUserEventList] = useState();
 
+  const backgroundStyle = {
+    background: "#F0C27B"
+  };
+
+  const jumbotronStyle = {
+    background: "#F0C27B",
+    textAlign: "center"
+  };
+
   useEffect(() => {
     if (userData) {
-      const { uid } = userData;
-      const unsubscribeUserEventList = firestore()
-        .collection("user_profile")
-        .doc(uid)
-        .collection("events")
-        .orderBy("startingTime")
-        .onSnapshot((snap) => {
-          let tmp = [];
-          snap.forEach((doc) => tmp.push(doc.data()));
-          setUserEventList(tmp);
-        });
-      return () => {
+      const {uid} = userData;
+      const unsubscribeUserEventList = firestore().collection("user_profile").doc(uid).collection("events").orderBy("startingTime").onSnapshot((snap) => {
+        let tmp = [];
+        snap.forEach((doc) => tmp.push(doc.data()));
+        setUserEventList(tmp);
+      });
+      return() => {
         unsubscribeUserEventList();
       };
     }
   }, [userData]);
 
   if (userLoading) {
-    return <Loading />;
+    return <Loading/>;
   } else if (!userData) {
-    return <Redirect to="/launch" />;
+    return <Redirect to="/launch"/>;
   } else if (!userEventList) {
-    return <Loading />;
+    return <Loading/>;
   } else {
-    return (
-      <div>
-        <NavBar />
-        <Jumbotron>
-          <h1>My Events</h1>
-          <p>Events I joined or hosted by me!</p>
-          <hr />
-          <Button block color="primary" tag={Link} to="/e/add">
-            Create a new event!
-          </Button>
-        </Jumbotron>
-        <div style={{ padding: "1rem" }}>
-          {userEventList &&
-            userEventList.length > 0 &&
-            userEventList.map((event) => (
-              <EventCard key={event.eid} eid={event.eid} />
-            ))}
-        </div>
+    return (<div style={backgroundStyle}>
+      <NavBar/>
+      <Jumbotron style={jumbotronStyle}>
+        <h1>My Events</h1>
+        <p>Events I joined or hosted by me!</p>
+        <hr/>
+        <Button block="block" color="primary" tag={Link} to="/e/add">
+          Create a new event!
+        </Button>
+      </Jumbotron>
+
+      <div style={{
+          padding: "1rem"
+        }}>
+        {userEventList && userEventList.length > 0 && userEventList.map((event) => (<EventCard key={event.eid} eid={event.eid}/>))}
       </div>
-    );
+    </div>);
   }
 };
 
--- code/dubby/src/components/Profile/ProfilePage.js
+++ code/dubby/src/components/Profile/ProfilePage.js
@@ -1,46 +1,31 @@
-import React, { useState, useContext, useEffect } from "react";
-import { auth, firestore } from "firebase";
-import { useParams, Redirect } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext";
-import { EventTypeContext } from "../../contexts/EventTypeContext";
-import { FriendContext } from "../../contexts/FriendContext";
-import { Button, Input, Form, Row, Col } from "reactstrap";
+import React, {useState, useContext, useEffect} from "react";
+import {auth, firestore} from "firebase";
+import {useParams, Redirect} from "react-router-dom";
+import {UserContext} from "../../contexts/UserContext";
+import {EventTypeContext} from "../../contexts/EventTypeContext";
+import {FriendContext} from "../../contexts/FriendContext";
+import {Button, Input, Form, Row, Col} from "reactstrap";
 import NavBar from "../Navbar/Navbar";
 import Loading from "../Loading/Loading";
 import EditProfileImage from "./EditProfileImage";
-import {
-  sendFriendRequest,
-  updateProfileData,
-  acceptFriendRequest,
-  unfriendFriend,
-  removeFriendRequest,
-} from "../../utilityfunctions/Utilities";
+import {sendFriendRequest, updateProfileData, acceptFriendRequest, unfriendFriend, removeFriendRequest} from "../../utilityfunctions/Utilities";
 import ProfileHead from "./ProfileHead";
 
 const ProfilePage = () => {
-  const { uid } = useParams();
+  const {uid} = useParams();
 
-  const { userData, userLoading } = useContext(UserContext);
-  const { eventTypeData } = useContext(EventTypeContext);
-  const {
-    sentRequestData,
-    receivedRequestData,
-    friendListData,
-    friendContextLoaded,
-  } = useContext(FriendContext);
+  const {userData, userLoading} = useContext(UserContext);
+  const {eventTypeData} = useContext(EventTypeContext);
+  const {sentRequestData, receivedRequestData, friendListData, friendContextLoaded} = useContext(FriendContext);
 
   const [profileData, setProfileData] = useState();
   const [isEditable, setIsEditable] = useState(false);
 
   useEffect(() => {
     if (uid) {
-      firestore()
-        .collection("user_profile")
-        .doc(uid)
-        .get()
-        .then((s) => {
-          setProfileData(s.data());
-        });
+      firestore().collection("user_profile").doc(uid).get().then((s) => {
+        setProfileData(s.data());
+      });
     } else {
       setProfileData(userData);
     }
@@ -51,14 +36,14 @@ const ProfilePage = () => {
   const handleProfileDataSubmit = async () => {
     toggleIsEditable();
     if (profileData !== userData) {
-      await updateProfileData({ profileData });
+      await updateProfileData({profileData});
     }
   };
 
   const handleProfileDataEdit = (key, value) => {
     const newProfileData = {
       ...profileData,
-      [key]: value,
+      [key]: value
     };
     setProfileData(newProfileData);
   };
@@ -70,195 +55,150 @@ const ProfilePage = () => {
   };
 
   if (userLoading) {
-    return <Loading />;
+    return <Loading/>;
   } else if (!userData) {
-    return <Redirect to="/launch" />;
+    return <Redirect to="/launch"/>;
   } else if (!profileData || !eventTypeData || !friendContextLoaded) {
-    return <Loading />;
+    return <Loading/>;
   } else if (uid !== auth().currentUser.uid && isEditable) {
     // in editing mode
-    const { username, description, university, profileImageSrc } = profileData;
-    return (
-      <div>
-        <NavBar />
-        <div style={{ marginBottom: "2rem" }}>
-          <ProfileHead src={profileImageSrc} size="profile" />
-          <br />
-          <Row>
-            <Col sm={{ size: 8, offset: 2 }}>
-              <Form onSubmit={handleProfileDataSubmit}>
-                <h2 style={{ marginTop: "50px" }}>Username</h2>
-                <hr />
-                <Input
-                  onChange={(e) =>
-                    handleProfileDataEdit("username", e.target.value)
-                  }
-                  value={username}
-                />
-                <h2 style={{ marginTop: "50px" }}>Personal Description</h2>
-                <hr />
-                <Input
-                  onChange={(e) =>
-                    handleProfileDataEdit("description", e.target.value)
-                  }
-                  value={description}
-                />
-                <h2 style={{ marginTop: "50px" }}>University</h2>
-                <hr />
-                <Input
-                  onChange={(e) =>
-                    handleProfileDataEdit("university", e.target.value)
-                  }
-                  value={university}
-                />
-                <h2 style={{ marginTop: "50px" }}>Sports</h2>
-                <hr />
-                <Input
-                  type="select"
-                  multiple
-                  onChange={handleInterestedSportsEdit}
-                >
-                  {eventTypeData.map(({ value, display }) => (
-                    <option key={value} value={value}>
-                      {display}
-                    </option>
-                  ))}
-                </Input>
-                <hr />
-                <EditProfileImage />
-                <hr />
-                <Button block type="submit">
-                  {profileData !== userData ? "Save" : "Cancel"}
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </div>
+    const {username, description, university, profileImageSrc} = profileData;
+    return (<div>
+      <NavBar/>
+      <div style={{
+          marginBottom: "2rem"
+        }}>
+        <ProfileHead src={profileImageSrc} size="profile"/>
+        <br/>
+        <Row>
+          <Col sm={{
+              size: 8,
+              offset: 2
+            }}>
+            <Form onSubmit={handleProfileDataSubmit}>
+              <h2 style={{
+                  marginTop: "50px"
+                }}>Username</h2>
+              <hr/>
+              <Input onChange={(e) => handleProfileDataEdit("username", e.target.value)} value={username}/>
+              <h2 style={{
+                  marginTop: "50px"
+                }}>Personal Description</h2>
+              <hr/>
+              <Input onChange={(e) => handleProfileDataEdit("description", e.target.value)} value={description}/>
+              <h2 style={{
+                  marginTop: "50px"
+                }}>University</h2>
+              <hr/>
+              <Input onChange={(e) => handleProfileDataEdit("university", e.target.value)} value={university}/>
+              <h2 style={{
+                  marginTop: "50px"
+                }}>Sports</h2>
+              <hr/>
+              <Input type="select" multiple="multiple" onChange={handleInterestedSportsEdit}>
+                {
+                  eventTypeData.map(({value, display}) => (<option key={value} value={value}>
+                    {display}
+                  </option>))
+                }
+              </Input>
+              <hr/>
+              <EditProfileImage/>
+              <hr/>
+              <Button block="block" type="submit">
+                {
+                  profileData !== userData
+                    ? "Save"
+                    : "Cancel"
+                }
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </div>
-    );
+    </div>);
   } else {
     // in viewing mode
     // button shows "add friend" if not self, otherwise show "edit"
-    const {
-      username,
-      description,
-      interested_sports,
-      university,
-      profileImageSrc,
-    } = profileData;
-    return (
-      <div>
-        <NavBar />
-        <div style={{ marginBottom: "2rem" }}>
-          <ProfileHead src={profileImageSrc} size="profile" />
-          <br />
-          <Row>
-            <Col sm={{ size: 8, offset: 2 }}>
-              <h2 style={{ marginTop: "50px" }}>Username</h2>
-              <hr />
-              <p>{username}</p>
-              <h2 style={{ marginTop: "50px" }}>Personal Description</h2>
-              <hr />
-              <p>{description}</p>
-              <h2 style={{ marginTop: "50px" }}>University</h2>
-              <hr />
-              <p>{university}</p>
-              <h2 style={{ marginTop: "50px" }}>Interested In</h2>
-              <hr />
-              {interested_sports.length &&
-              interested_sports.length > 0 &&
-              interested_sports.map !== undefined ? (
-                <ul>
-                  {interested_sports.map((s) => (
-                    <li key={s}>
-                      {eventTypeData.find((c) => c.display === s) && (
-                        <i
-                          className={
-                            eventTypeData.find((c) => c.display === s).icon
-                          }
-                        ></i>
-                      )}{" "}
+    const {username, description, interested_sports, university, profileImageSrc} = profileData;
+    return (<div>
+      <NavBar/>
+      <div style={{
+          marginBottom: "2rem"
+        }}>
+        <ProfileHead src={profileImageSrc} size="profile"/>
+        <br/>
+        <Row>
+          <Col sm={{
+              size: 8,
+              offset: 2
+            }}>
+            <h2 style={{
+                marginTop: "50px"
+              }}>Username</h2>
+            <hr/>
+            <p>{username}</p>
+            <h2 style={{
+                marginTop: "50px"
+              }}>Personal Description</h2>
+            <hr/>
+            <p>{description}</p>
+            <h2 style={{
+                marginTop: "50px"
+              }}>University</h2>
+            <hr/>
+            <p>{university}</p>
+            <h2 style={{
+                marginTop: "50px"
+              }}>Interested In</h2>
+            <hr/> {
+              interested_sports.length && interested_sports.length > 0 && interested_sports.map !== undefined
+                ? (<ul>
+                  {
+                    interested_sports.map((s) => (<li key={s}>
+                      {
+                        eventTypeData.find((c) => c.display === s) && (<i className={eventTypeData.find((c) => c.display === s).icon
+}></i>)
+                      }{" "}
                       {s}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p>{interested_sports}</p>
-              )}
-              <hr />
-              <ProfileActionButton
-                uid={uid}
-                toggleIsEditable={toggleIsEditable}
-                friendListData={friendListData}
-                sentRequestData={sentRequestData}
-                receivedRequestData={receivedRequestData}
-              />
-            </Col>
-          </Row>
-        </div>
+                    </li>))
+                  }
+                </ul>)
+                : (<p>{interested_sports}</p>)
+            }
+            <hr/>
+            <ProfileActionButton uid={uid} toggleIsEditable={toggleIsEditable} friendListData={friendListData} sentRequestData={sentRequestData} receivedRequestData={receivedRequestData}/>
+          </Col>
+        </Row>
       </div>
-    );
+    </div>);
   }
 };
 
-const ProfileActionButton = ({
-  uid,
-  toggleIsEditable,
-  friendListData,
-  sentRequestData,
-  receivedRequestData,
-}) => {
+const ProfileActionButton = ({uid, toggleIsEditable, friendListData, sentRequestData, receivedRequestData}) => {
   if (uid && uid !== auth().currentUser.uid) {
     if (friendListData && friendListData.find((p) => p.uid === uid)) {
-      return (
-        <Button
-          block
-          color="danger"
-          onClick={() => unfriendFriend({ targetUid: uid })}
-        >
-          Unfriend
-        </Button>
-      );
+      return (<Button block="block" color="danger" onClick={() => unfriendFriend({targetUid: uid})}>
+        Unfriend
+      </Button>);
     } else if (sentRequestData && sentRequestData.find((p) => p.uid === uid)) {
-      return (
-        <Button
-          block
-          color="warning"
-          onClick={() => removeFriendRequest({ targetUid: uid })}
-        >
-          Unrequest
-        </Button>
-      );
-    } else if (
-      receivedRequestData &&
-      receivedRequestData.find((p) => p.uid === uid)
-    ) {
-      return (
-        <Button
-          block
-          color="primary"
-          onClick={() => acceptFriendRequest({ targetUid: uid })}
-        >
-          Confirm Friend
-        </Button>
-      );
+      return (<Button block="block" color="warning" onClick={() => removeFriendRequest({targetUid: uid})}>
+        Unrequest
+      </Button>);
+    } else if (receivedRequestData && receivedRequestData.find((p) => p.uid === uid)) {
+      return (<Button block="block" color="primary" onClick={() => acceptFriendRequest({targetUid: uid})}>
+        Confirm Friend
+      </Button>);
     } else {
-      return (
-        <Button
-          block
-          color="primary"
-          onClick={() => sendFriendRequest({ targetUid: uid })}
-        >
-          Add Friend
-        </Button>
-      );
+      return (<Button block="block" color="primary" onClick={() => sendFriendRequest({targetUid: uid})}>
+        Add Friend
+      </Button>);
     }
-  } else
-    return (
-      <Button block onClick={() => toggleIsEditable()}>
-        Edit
-      </Button>
-    );
-};
+  } else 
+    return (<Button block="block" onClick={() => toggleIsEditable()}>
+      Edit
+    </Button>);
+  }
+;
 
 export default ProfilePage;
