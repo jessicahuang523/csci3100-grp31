e18f930a5c58a196b5b704edd5985006da6bb443
Lawrence Tsai <tsailawrence812@gmail.com>
2020-04-21 15:24:43 +0800

    Update LoadingUserCreation.js

--- code/dubby/src/components/Loading/LoadingUserCreation.js
+++ code/dubby/src/components/Loading/LoadingUserCreation.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Spinner } from "reactstrap";
+import React, {useState, useEffect} from "react";
+import {Spinner} from "reactstrap";
 
 const messages = [
   "Hang on! We're getting your data ready...",
   "Just a sec! it's coming!",
+  "You will love Dubby, just like I do",
   "Whoa, this is taking longer than expected...",
-  "Wiiiiiiii",
+  "By the way have you eaten lunch yet? Dinner?",
+  "I love hamburgers!",
+  "Wiiiiiiii"
 ];
 
 const LoadingUserCreation = () => {
@@ -15,26 +18,22 @@ const LoadingUserCreation = () => {
     const clear = setInterval(() => {
       setLoadingMessage((loadingMessage + 1) % messages.length);
     }, 3000);
-    return () => {
+    return() => {
       clearInterval(clear);
     };
   }, [loadingMessage]);
 
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Spinner color="warning" />
-      <p>{messages[loadingMessage]}</p>
-    </div>
-  );
+  return (<div style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+    <Spinner color="warning"/>
+    <p>{messages[loadingMessage]}</p>
+  </div>);
 };
 
 export default LoadingUserCreation;
